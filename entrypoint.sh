48c451f17386d69be80cede1fa15bbb250698825
Matthew Mckenzie <mmckenzie@vostronet.com>
2017-07-23 15:16:31 +1000

    perms fix?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 groupmod -g $(stat -c "%g" /var/run/docker.sock) docker
-#usermod -u $(stat -c "%u" /var/jenkins_home) jenkins
+chown -R jenkins /var/jenkins_home
 su-exec jenkins /bin/tini -- /usr/local/bin/jenkins.sh
